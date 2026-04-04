96a00e4928d099670fab87b04347214dd495df28
Hoang Anh <htheanh2004@gmail.com>
2026-04-04 21:54:48 +0700

    add some library

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get<string>('PORT');
+  app.setGlobalPrefix('api');
+  
+  app.use(cookieParser());
+
+  const configSwagger =  new DocumentBuilder()
+    .setTitle('MediPath API')
+    .setDescription('The MediPath API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, configSwagger);
+  SwaggerModule.setup('api', app, document);
+
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    credentials: true,
+  });
+
+  await app.listen(port ?? 3000);
 }
 bootstrap();
