0eeb83babe9d611f7101860e657fc522563cae46
Doan Chi Dung <chidung7271@gmail.com>
2026-04-08 00:05:49 +0700

    fix(be): apply JwtAuthGuard APP_GUARD, update admin routes, and align Register/User DTOs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core/constants';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+import { AuthModule } from './modules/auth/auth.module';
+import { JwtAuthGuard } from './modules/auth/gaurds/jwt-auth.guard';
 import { CourseModuleModule } from './modules/course-module/course-module.module';
 import { CourseSkillModule } from './modules/course-skill/course-skill.module';
 import { CourseModule } from './modules/course/course.module';
@@ -14,8 +18,6 @@ import { PostModule } from './modules/post/post.module';
 import { SkillModule } from './modules/skill/skill.module';
 import { UserModule } from './modules/user/user.module';
 import { UserLogModule } from './modules/user_log/user_log.module';
-import { AuthModule } from './modules/auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -46,6 +48,10 @@ import { ConfigModule } from '@nestjs/config';
   ],
     controllers: [AppController],
   providers: [AppService,
+        {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
   ],
 })
 export class AppModule implements NestModule {
