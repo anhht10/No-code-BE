1caf7468744564a39229b811958fb7f2743963f0
Tlammmm <nguyenthailam.qng2020@gmail.com>
2026-04-10 17:01:41 +0700

    feat(schedules-module): fix import

--- src/modules/post-schedules/post-schedules.module.ts
+++ src/modules/post-schedules/post-schedules.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PostSchedulesService } from './post-schedules.service';
 import { PostSchedulesController } from './post-schedules.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PostSchedule, PostScheduleSchema } from './schemas/post-schedule.schema';
 
 @Module({
+  imports : [
+    MongooseModule.forFeature([{ name: PostSchedule.name, schema: PostScheduleSchema }]),
+  ], 
   controllers: [PostSchedulesController],
   providers: [PostSchedulesService],
 })
